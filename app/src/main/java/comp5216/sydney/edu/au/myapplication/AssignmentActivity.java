8bcbbddcb578706643b188ef9824cfa3c85e4e80
barrytong <371202041@qq.com>
2021-10-16 15:49:45 +0800

    Task Page 5 Update

--- app/src/main/java/comp5216/sydney/edu/au/myapplication/AssignmentActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/myapplication/AssignmentActivity.java
@@ -31,15 +31,8 @@ public class AssignmentActivity extends AppCompatActivity {
     private Button profile;
     private RecyclerView recyclerView;
     private AssignmentAdapter adapter;
-    private List<AssignmentModel> mList;
-    private List<TaskModel> list;
-    private List<TaskModel> list1;
-    private List<TaskModel> list2;
     private List<Task> taskFromFireBase;
-    private List<TaskOfAssignmentModel> taskList;
     private FirebaseFirestore firestore;
-
-
 
 
     @Override
@@ -54,48 +47,20 @@ public class AssignmentActivity extends AppCompatActivity {
         firestore = FirebaseFirestore.getInstance();
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(AssignmentActivity.this));
-        mList = new ArrayList<>();
-        list = new ArrayList<>();
-        list1 = new ArrayList<>();
-        list2 = new ArrayList<>();
-        taskList = new ArrayList<>();
         taskFromFireBase = new ArrayList<>();
-
-        TaskModel t1 = new TaskModel("Barry", "IT", "Java,Vue", 0);
-        TaskModel t2 = new TaskModel("Shela", "CV Engineering", "None", 0);
-        TaskModel t3 = new TaskModel("Tim", "IT", "C++, Web Develop", 0);
-        TaskModel t4 = new TaskModel("Sam", "IT", "C++, Java", 0);
-        list.add(t1);
-        list1.add(t1);
-        list2.add(t1);
-        list1.add(t2);
-        list2.add(t3);
-        TaskOfAssignmentModel task1 = new TaskOfAssignmentModel("App Part 5","Develop UniTask Part 5 From Proposal",list,"COMP5216 Group 18","1/11/2021",0);
-        TaskOfAssignmentModel task2 = new TaskOfAssignmentModel("App Part 8","Develop UniTask Part 8 From Proposal",list1,"COMP9103","2/11/2021",0);
-        TaskOfAssignmentModel task3 = new TaskOfAssignmentModel("App Part 9","Develop UniTask Part 9 From Proposal",list2,"COMP9120","3/11/2021",0);
-        taskList.add(task1);
-        taskList.add(task2);
-        taskList.add(task3);
-        AssignmentModel a1 = new AssignmentModel("COMP5216 Group 18","Build UniTask","13/7/2021",taskList,0);
-        AssignmentModel a2 = new AssignmentModel("COMP9103","Java Basic","14/8/2021",taskList,0);
-        AssignmentModel a3 = new AssignmentModel("COMP9120","SQL Introduction","14/8/2021",taskList,0);
-        mList.add(a1);
-        mList.add(a2);
-        mList.add(a3);
         showData();
 
 
-
-        adapter = new AssignmentAdapter(taskFromFireBase,AssignmentActivity.this);
+        adapter = new AssignmentAdapter(taskFromFireBase, AssignmentActivity.this);
         recyclerView.setAdapter(adapter);
     }
 
-    public void toHome(View view){
-        Intent intent = new Intent( AssignmentActivity.this, MainActivity.class);
+    public void toHome(View view) {
+        Intent intent = new Intent(AssignmentActivity.this, MainActivity.class);
         startActivity(intent);
     }
 
-    public void toTask(View view){
+    public void toTask(View view) {
         Intent intent = new Intent(AssignmentActivity.this, CreateAssignmentActivity.class);
         startActivity(intent);
     }
@@ -106,7 +71,7 @@ public class AssignmentActivity extends AppCompatActivity {
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
                 for (DocumentChange documentChange : value.getDocumentChanges()) {
                     if (documentChange.getType() == DocumentChange.Type.ADDED) {
-                        Task taskModel =  documentChange.getDocument().toObject(Task.class);
+                        Task taskModel = documentChange.getDocument().toObject(Task.class);
                         taskFromFireBase.add(taskModel);
                         adapter.notifyDataSetChanged();
                     }
